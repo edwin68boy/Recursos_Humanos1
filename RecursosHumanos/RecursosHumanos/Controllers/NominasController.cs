1a8d3a5a6fa7b740750cb559cc802080326eab54
EDWIN POUERIET <edwin5897.oort@gmail.com>
2019-04-07 13:46:09 -0400

    summit antes de la playa

---
+++ RecursosHumanos/RecursosHumanos/Controllers/NominasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RecursosHumanos.Models;
+
+namespace RecursosHumanos.Controllers
+{
+    public class NominasController : Controller
+    {
+        private DatabaseEntities db = new DatabaseEntities();
+
+        // GET: Nominas
+        public ActionResult Index()
+        {
+            return View(db.Nominas.ToList());
+        }
+
+        // GET: Nominas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nomina nomina = db.Nominas.Find(id);
+            if (nomina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nomina);
+        }
+
+        // GET: Nominas/Create
+        public ActionResult Create()
+        {
+
+            ViewBag.TotalEmpleados = (from a in db.Empleados
+                                      where a.Estatus == true
+                                      select a.Salario).Count();
+
+            ViewBag.TotalSalario = (from a in db.Empleados where a.Estatus == true
+                         select a.Salario ).Sum();
+
+
+            return View();
+        }
+
+        // POST: Nominas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Fecha,Monto_Total")] Nomina nomina)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Nominas.Add(nomina);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(nomina);
+        }
+
+        // GET: Nominas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nomina nomina = db.Nominas.Find(id);
+            if (nomina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nomina);
+        }
+
+        // POST: Nominas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Fecha,Monto_Total")] Nomina nomina)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nomina).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nomina);
+        }
+
+        // GET: Nominas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nomina nomina = db.Nominas.Find(id);
+            if (nomina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nomina);
+        }
+
+        // POST: Nominas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Nomina nomina = db.Nominas.Find(id);
+            db.Nominas.Remove(nomina);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
